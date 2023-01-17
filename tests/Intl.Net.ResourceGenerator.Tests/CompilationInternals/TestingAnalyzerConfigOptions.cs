aaa18a1efc1f4a899db946a03322006cf767cf2e
curtisy <curtisy@mailbox.org>
2023-01-17 21:29:59 +0100

    fix(tests): disable tolgee plugin

--- tests/Intl.Net.ResourceGenerator.Tests/CompilationInternals/TestingAnalyzerConfigOptions.cs
+++ tests/Intl.Net.ResourceGenerator.Tests/CompilationInternals/TestingAnalyzerConfigOptions.cs
@@ -1,10 +1,18 @@
+using System.Linq;
+
 namespace Intl.Net.ResourceGenerator.Tests.CompilationInternals;
 
+using System.Collections.Generic;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 public class TestingAnalyzerConfigOptions : AnalyzerConfigOptions {
+  private readonly List<string> falsyKeys = new List<string> {
+    "ResourceUri",
+    "EnableTolgeeProvider",
+  };
+  
   public override bool TryGetValue(string key, out string? value) {
     value = "Intl.Net.Testing";
-    return !key.Contains("ResourceLocation");
+    return !falsyKeys.Any(key.Contains);
   }
 }
